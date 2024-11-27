97354cadaf1f24c6863cc7f5b9c73d5e9fa8e927
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-26 22:22:43 -0700

    updated file name. strating on database connetion and character retrieval

---
+++ Backend/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace Backend
+{
+    public class Database
+    {
+        private static readonly string connectionString = "Server=your_server_name;Database=your_database_name;Trusted_Connection=True;";
+
+
+
+    }
+}
