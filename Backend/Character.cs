94d26da6a8793a490a84945f18d7758ca8580295
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-26 22:37:50 -0700

    adding a simple class to start to get back to getAllCharacters()

---
+++ Backend/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public string AbilityScores { get; set; }
+
+
+
+    }
+}
