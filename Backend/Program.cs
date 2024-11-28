a19f116738bfb729f417ac9f6e9ad34b51fcaf15
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-27 23:02:09 -0700

    updating names to follow GET concept

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -9,7 +9,7 @@ namespace Backend
             Console.WriteLine("Let's fetch all Characters...");
 
             // Step 1: Query raw data from Database 
-            var rawData = Database.FetchRawDataFromDatabase("SELECT * FROM Characters");
+            var rawData = Database.GetRawDataFromDatabase("SELECT * FROM Characters");
 
             // Step 2: Map raw data to the Character objects 
             var characters = CharacterMapper.MapToCharacters(rawData);
