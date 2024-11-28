124042588d28d7c9f2fa8d48f0f3c99f220a1cf8
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-27 23:03:45 -0700

    adding the character controller to handle mapper, database and character obj classes

---
+++ Backend/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class CharacterController
+    {
+        private readonly Database _database;
+        private readonly CharacterMapper _mapper;
+
+        public CharacterController(Database database, CharacterMapper mapper)
+        {
+            _database = database;
+            _mapper = mapper;
+        }
+
+
+    }
+}
