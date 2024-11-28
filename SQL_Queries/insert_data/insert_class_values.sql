f56a1343adce91903af03127d915371180722b43
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-28 10:31:46 -0700

    queries for inserting data into tables for now

---
+++ SQL_Queries/insert_data/insert_class_values.sql
@@ -0,0 +1,4 @@
+INSERT INTO Classes (ClassID, ClassName, HitDie, PrimaryAbility, SavingThrows, SKillChoices, ClassFeatures)
+VALUES
+(1, 'Fighter', 'd10', 'Strength', '["Strength", "Constitution"]','["Athletics", "Survival"]', '["Second Wind", "Action Surge"]'),
+(2, 'Wizard', 'd6', 'Intelligence', '["Intelligence", "Wisdom"]', '["Arcana", "History"]', '["Spellcasting", "Arcane Recovery"]');
---
+++ SQL_Queries/insert_data/insert_race_values.sql
@@ -0,0 +1,4 @@
+INSERT INTO Races (RaceID, RaceName, RaceSize, Speed, AbilityScoreBonuses, Languages, RacialFeatures)
+VALUES
+(1, 'Human', 'Medium', 30, '{"Strength": 1, "Dexterity": 1}', '["Common"]','["Versatility"]'),
+(2, 'Elf', 'Medium', 30, '{"Dexterity": 2, "Wisdom": 1}', '["Common","Elvish"]', '["Darkvision", "Fey Ancestry"]');
