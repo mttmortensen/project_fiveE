c153ebb4c1dff400660509a32e861cd83f0370eb
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-27 23:13:48 -0700

    Updating program to be router and now handling get requests for /characters

---
+++ Backend/Router.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Backend
+{
+    public static class Router
+    {
+        public static string HandleRequest(HttpListenerRequest request) 
+        {
+            // Check the request's path 
+            if (request.HttpMethod == "GET" && request.Url.AbsolutePath == "/characters") 
+            {
+                // 1. Get controller ready to get all the characters 
+                var controller = new CharacterController(new Database(), new CharacterMapper());
+                var characters = controller.GetAllCharacters();
+
+                // 2. Wrap the response in a JSON string 
+                return JsonSerializer.Serialize(characters);
+            }
+
+            return "404 not found";
+        }
+    }
+}
