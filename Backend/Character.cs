21d2bb0af72fff11866cd5565deb449ee0ac1aea
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-28 22:43:30 -0700

    props set for Race class

--- Backend/Character.cs
+++ Backend/Character.cs
@@ -36,6 +36,13 @@
 
     public class Race 
     { 
+        public int Id { get; set; }
+        public string RaceName { get; set; }
+        public string RaceSize { get; set; }
+        public string Speed { get; set; }
+        public List<string> AbilityScoreBonuses { get; set; }
+        public List<string> Languages { get; set; }
+        public List<string> RacialFeatures { get; set; }
 
     }
 
