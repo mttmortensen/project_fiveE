4fd570d3788eb87ae8a15ac57f220c14c34d0cf0
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-28 22:41:49 -0700

    properties set for Classes

--- Backend/Character.cs
+++ Backend/Character.cs
@@ -23,6 +23,14 @@
 
     public class Classes
     {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
+        public string HitDie { get; set; }
+        public string PrimaryAbility { get; set; }
+        public List<string> SavingThrows { get; set; }
+        public List<string> SkillChoices { get; set; }
+        public List<string> ClassFeatures { get; set; }
+
 
     }
 
