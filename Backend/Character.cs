775d76d77284dc00dbcf4f12e989cf504901167e
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-28 20:37:08 -0700

    udpating types for the Character class

--- Backend/Character.cs
+++ Backend/Character.cs
@@ -5,12 +5,12 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Sex { get; set; }
-        public string Class { get; set; }
-        public string Race { get; set; }
-        public string AbilityScores { get; set; }
-        public string Skills { get; set; }
-        public string Proficiencies { get; set; }
-        public string Equipment { get; set; }
+        public Classes Classes { get; set; }
+        public Race Race { get; set; }
+        public List<AbilityScores> AbilityScores { get; set; } = new List<AbilityScores>();
+        public List<string> Skills { get; set; } = new List<string>();
+        public List<string> Proficiencies { get; set; } = new List<string>();
+        public List<string> Equipment { get; set; } = new List<string>();
         public string Background { get; set; }
         public string Alignment { get; set; }
         public int Level { get; set; }
