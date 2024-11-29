6bf7c468c22091b161057c9cec583bc9d59d255c
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-28 18:07:51 -0700

    added all props to the character class matching the character db

--- Backend/Character.cs
+++ Backend/Character.cs
@@ -10,9 +10,22 @@ namespace Backend
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Sex { get; set; }
         public string Class { get; set; }
+        public string Race { get; set; }
+        public string Skills { get; set; }
+        public string Proficiencies { get; set; }
+        public string Equipment { get; set; }
+        public string Background { get; set; }
+        public string Alignment { get; set; }
         public int Level { get; set; }
         public int HP { get; set; }
+        public int XP { get; set; }
+        public int MaxHP { get; set; }
+        public int Speed { get; set; }
+        public int AC { get; set; }
         public string AbilityScores { get; set; }
+
+
     }
 }
