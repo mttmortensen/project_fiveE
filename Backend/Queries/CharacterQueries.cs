940a058c3c78e4538ecc8e244f8593aea3bd1c2c
mttmortensen <mtt.mortensen527@gmail.com>
2024-12-01 09:44:42 -0700

    wrote out JOIN statements

--- Backend/Queries/CharacterQueries.cs
+++ Backend/Queries/CharacterQueries.cs
@@ -16,7 +16,10 @@ namespace Backend
                    r.RaceName, r.RaceSize, r.Speed AS RaceSpeed, r.AbilityScoreBonuses, r.Languages, r.RacialFeatures,
                    cl.ClassName, cl.HitDie, cl.PrimaryAbility, cl.SavingThrows, cl.SkillChoices, cl.ClassFeatures,
                    a.Strength, a.Dexterity, a.Constitution, a.Intelligence, a.Wisdom, a.Charisma
-
+            FROM Characters c
+            LEFT JOIN Races r ON c.RaceID = r.RaceID
+            LEFT JOIN Classes cl ON c.ClassID = cl.ClassID
+            LEFT JOIN AbilityScores a ON c.AbilityID = a.AbilityID;
         "; 
     }
 }
