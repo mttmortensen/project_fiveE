307c1d12ecdcec2a3588468f4b6f8f44f6770c8a
mttmortensen <mtt.mortensen527@gmail.com>
2024-12-01 09:42:16 -0700

    start of first query. Adding comments

---
+++ Backend/Queries/CharacterQueries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    // This class is write out the complex queries when dealing with the Character tables
+    // AND any other related table data associated with it.
+    public static class CharacterQueries
+    {
+        // Using a lambda getter expersion here
+        public static string GetAllCharacterAndItsRelatedData => ""; 
+    }
+}
