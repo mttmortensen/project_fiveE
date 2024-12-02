495144f79af3f1570f85e9c45ed47d33b2b46e04
mttmortensen <mtt.mortensen527@gmail.com>
2024-12-02 12:32:44 -0700

    doing basic label insertion and updates

--- Frontend/Form1.Designer.cs
+++ Frontend/Form1.Designer.cs
@@ -29,21 +29,36 @@
         private void InitializeComponent()
         {
             character_title = new Label();
+            character_info = new Label();
             SuspendLayout();
             // 
             // character_title
             // 
-            character_title.Location = new Point(9, 7);
+            character_title.Dock = DockStyle.Top;
+            character_title.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            character_title.Location = new Point(0, 0);
             character_title.Name = "character_title";
-            character_title.Size = new Size(258, 39);
+            character_title.Size = new Size(800, 39);
             character_title.TabIndex = 0;
             character_title.Text = "Title";
+            // 
+            // character_info
+            // 
+            character_info.Dock = DockStyle.Top;
+            character_info.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            character_info.Location = new Point(0, 39);
+            character_info.Name = "character_info";
+            character_info.Size = new Size(800, 39);
+            character_info.TabIndex = 1;
+            character_info.Text = "Character Info:";
+            character_info.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(character_info);
             Controls.Add(character_title);
             Name = "Form1";
             Text = "Form1";
@@ -54,5 +69,6 @@
 
         private Label character_title;
         public Label label1;
+        private Label character_info;
     }
 }
