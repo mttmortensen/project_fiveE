71339e5f34e95666957071589ae73219ceffda1f
mttmortensen <mtt.mortensen527@gmail.com>
2024-12-02 21:45:33 -0700

    understanding anchor points better

--- Frontend/Form1.Designer.cs
+++ Frontend/Form1.Designer.cs
@@ -39,12 +39,16 @@
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.ColumnCount = 5;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.9992046F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.9992F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.9992F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.9992F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20.0032F));
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 8;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.4995F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.4995F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.4995F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.4995F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5020008F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
             tableLayoutPanel1.Controls.Add(sex_label, 2, 0);
             tableLayoutPanel1.Controls.Add(textBox1, 1, 0);
             tableLayoutPanel1.Controls.Add(sex_m_rButton, 3, 0);
@@ -62,29 +66,30 @@
             // 
             sex_label.Anchor = AnchorStyles.Top;
             sex_label.AutoSize = true;
-            sex_label.Font = new Font("Courier New", 24F, FontStyle.Bold);
-            sex_label.Location = new Point(344, 0);
+            sex_label.Font = new Font("Segoe UI", 12F);
+            sex_label.Location = new Point(224, 0);
             sex_label.Name = "sex_label";
-            sex_label.Size = new Size(91, 36);
+            sex_label.Size = new Size(37, 21);
             sex_label.TabIndex = 2;
             sex_label.Text = "Sex:";
             // 
             // textBox1
             // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             textBox1.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(159, 3);
+            textBox1.Location = new Point(100, 3);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "First, Last";
-            textBox1.Size = new Size(150, 22);
+            textBox1.Size = new Size(91, 22);
             textBox1.TabIndex = 4;
             // 
             // sex_m_rButton
             // 
             sex_m_rButton.AutoSize = true;
-            sex_m_rButton.Font = new Font("Courier New", 20.25F);
-            sex_m_rButton.Location = new Point(471, 3);
+            sex_m_rButton.Font = new Font("Segoe UI", 12F);
+            sex_m_rButton.Location = new Point(294, 3);
             sex_m_rButton.Name = "sex_m_rButton";
-            sex_m_rButton.Size = new Size(95, 34);
+            sex_m_rButton.Size = new Size(62, 25);
             sex_m_rButton.TabIndex = 5;
             sex_m_rButton.TabStop = true;
             sex_m_rButton.Text = "Male";
@@ -93,10 +98,10 @@
             // sex_f_button
             // 
             sex_f_button.AutoSize = true;
-            sex_f_button.Font = new Font("Courier New", 20.25F);
-            sex_f_button.Location = new Point(627, 3);
+            sex_f_button.Font = new Font("Segoe UI", 12F);
+            sex_f_button.Location = new Point(391, 3);
             sex_f_button.Name = "sex_f_button";
-            sex_f_button.Size = new Size(127, 34);
+            sex_f_button.Size = new Size(78, 25);
             sex_f_button.TabIndex = 3;
             sex_f_button.TabStop = true;
             sex_f_button.Text = "Female";
@@ -106,10 +111,10 @@
             // 
             character_label.Anchor = AnchorStyles.Top;
             character_label.AutoSize = true;
-            character_label.Font = new Font("Courier New", 24F, FontStyle.Bold);
-            character_label.Location = new Point(23, 0);
+            character_label.Font = new Font("Segoe UI", 12F);
+            character_label.Location = new Point(21, 0);
             character_label.Name = "character_label";
-            character_label.Size = new Size(110, 36);
+            character_label.Size = new Size(55, 21);
             character_label.TabIndex = 0;
             character_label.Text = "Name:";
             // 
