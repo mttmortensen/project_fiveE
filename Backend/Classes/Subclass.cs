d69a828789c1309ed5ee535fd8d0f5339caf42ab
mttmortensen <mtt.mortensen527@gmail.com>
2025-04-28 20:40:23 -0600

    added starting subclass props

--- Backend/Classes/Subclass.cs
+++ Backend/Classes/Subclass.cs
@@ -8,5 +8,10 @@ namespace Backend.Classes
 {
     internal class Subclass
     {
+        public int SubclassID { get; set; }
+        public int ClassID { get; set; } // FK to Classes
+        public string SubclassName { get; set; }
+        public int EntryLevel { get; set; } = 3; // Most subclasses kick in at level 3
+
     }
 }
