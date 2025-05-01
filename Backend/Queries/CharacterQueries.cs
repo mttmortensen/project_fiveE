e9410837aa03fedcd9aa09aeb65e2f194f6ed487
mttmortensen <mtt.mortensen527@gmail.com>
2025-04-30 20:01:10 -0600

    character query to get the character by Id

--- Backend/Queries/CharacterQueries.cs
+++ Backend/Queries/CharacterQueries.cs
@@ -44,6 +44,24 @@
             LEFT JOIN Abilities a ON c.AbilityID = a.AbilityID;
         ";
 
+        public string GetSingleCharacterAndRelatedData => @"
+            SELECT 
+                c.*, 
+                r.RaceName, r.RaceSize, r.Speed AS RaceSpeed, 
+                r.AbilityScoreBonuses, r.Languages, r.RacialFeatures, r.RacialProficiencies, r.Darkvision,
+                cl.ClassName, cl.HitDie, cl.PrimaryAbility, cl.SavingThrows, cl.SkillChoices, cl.ClassFeatures, 
+                cl.ArmorProficiencies, cl.WeaponProficiencies, cl.ToolProficiencies, cl.SpellcastingAbilityModifier,
+                sc.SubclassName, sc.EntryLevel, sc.SubclassFeatures, sc.BonusProficiencies, sc.BonusSpells,
+                a.Strength, a.Dexterity, a.Constitution, a.Intelligence, a.Wisdom, a.Charisma
+            FROM Characters c
+            LEFT JOIN Races r ON c.RaceID = r.RaceID
+            LEFT JOIN Classes cl ON c.ClassID = cl.ClassID
+            LEFT JOIN Subclasses sc ON c.SubclassID = sc.SubclassID
+            LEFT JOIN Abilities a ON c.AbilityID = a.AbilityID
+            WHERE c.CharacterID = @CharacterID;
+        ";
+
+
 
         // Needing to get the CharacterID first before creating the Ability query 
         public string AddingNewCharacterWithoutAbilityId => @"
