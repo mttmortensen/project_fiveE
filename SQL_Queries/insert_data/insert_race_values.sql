8e88ab55f629be830160246f4438d56e132a78eb
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-03 22:18:51 -0600

    including now all races from dnd 5e

--- SQL_Queries/insert_data/insert_race_values.sql
+++ SQL_Queries/insert_data/insert_race_values.sql
@@ -1,4 +1,30 @@
-INSERT INTO Races (RaceID, RaceName, RaceSize, Speed, AbilityScoreBonuses, Languages, RacialFeatures)
+INSERT INTO [DNDCharacterDB].[dbo].[Races] (
+    RaceName, RaceSize, Speed, Languages, RacialFeatures
+)
 VALUES
-(1, 'Human', 'Medium', 30, '{"Strength": 1, "Dexterity": 1}', '["Common"]','["Versatility"]'),
-(2, 'Elf', 'Medium', 30, '{"Dexterity": 2, "Wisdom": 1}', '["Common","Elvish"]', '["Darkvision", "Fey Ancestry"]');
+-- 1. Human
+('Human', 'Medium', 30, '["Common"]', '["Versatility"]'),
+
+-- 2. Elf
+('Elf', 'Medium', 30, '["Common", "Elvish"]', '["Darkvision", "Fey Ancestry", "Keen Senses", "Trance"]'),
+
+-- 3. Dwarf
+('Dwarf', 'Medium', 25, '["Common", "Dwarvish"]', '["Darkvision", "Dwarven Resilience", "Stonecunning"]'),
+
+-- 4. Halfling
+('Halfling', 'Small', 25, '["Common", "Halfling"]', '["Lucky", "Brave", "Halfling Nimbleness"]'),
+
+-- 5. Dragonborn
+('Dragonborn', 'Medium', 30, '["Common", "Draconic"]', '["Draconic Ancestry", "Breath Weapon", "Damage Resistance"]'),
+
+-- 6. Gnome
+('Gnome', 'Small', 25, '["Common", "Gnomish"]', '["Darkvision", "Gnome Cunning"]'),
+
+-- 7. Half-Elf
+('Half-Elf', 'Medium', 30, '["Common", "Elvish"]', '["Darkvision", "Fey Ancestry", "Skill Versatility"]'),
+
+-- 8. Half-Orc
+('Half-Orc', 'Medium', 30, '["Common", "Orc"]', '["Darkvision", "Relentless Endurance", "Savage Attacks"]'),
+
+-- 9. Tiefling
+('Tiefling', 'Medium', 30, '["Common", "Infernal"]', '["Darkvision", "Hellish Resistance", "Infernal Legacy"]');
