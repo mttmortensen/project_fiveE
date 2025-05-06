9de5c9dbff0718dcd7c501734d217b6eaab9438f
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 09:02:54 -0600

    adding controller for race data handling

--- Backend/Controllers/RaceController.cs
+++ Backend/Controllers/RaceController.cs
@@ -8,5 +8,21 @@ namespace Backend
 {
     public class RaceController
     {
+        private readonly Database _database;
+        private readonly RaceQueries _queries;
+        private readonly RaceMapper _mapper;
+
+        public RaceController(Database database) 
+        {
+            _database = database;
+            _queries = new RaceQueries();
+            _mapper = new RaceMapper();
+        }
+
+        public List<Race> GetAllRaces()
+        {
+            var rawData = _database.GetRawDataFromDatabase(_queries.GetAllRaces);
+            return _mapper.MapToRaceList(rawData);
+        }
     }
 }
