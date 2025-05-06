a84619ce7677273c81fd6d2088542d60b4eb6d65
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-05 20:53:40 -0600

    adding of step 3 for class and subclass

---
+++ Frontend/Step3.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Frontend
+{
+    partial class Step3
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblClass;
+        private ComboBox cmbClass;
+        private Label lblSubclass;
+        private ComboBox cmbSubclass;
+
+        private void InitializeComponent()
+        {
+            this.lblClass = new Label();
+            this.cmbClass = new ComboBox();
+            this.lblSubclass = new Label();
+            this.cmbSubclass = new ComboBox();
+
+            this.SuspendLayout();
+
+            // Label: Class
+            this.lblClass.Text = "Select Class:";
+            this.lblClass.Location = new System.Drawing.Point(20, 20);
+
+            // ComboBox: Class
+            this.cmbClass.Location = new System.Drawing.Point(120, 20);
+            this.cmbClass.Width = 180;
+            this.cmbClass.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.cmbClass.SelectedIndexChanged += new System.EventHandler(this.cmbClass_SelectedIndexChanged);
+
+            // Label: Subclass
+            this.lblSubclass.Text = "Select Subclass:";
+            this.lblSubclass.Location = new System.Drawing.Point(20, 60);
+
+            // ComboBox: Subclass
+            this.cmbSubclass.Location = new System.Drawing.Point(120, 60);
+            this.cmbSubclass.Width = 180;
+            this.cmbSubclass.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Add controls
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.cmbClass);
+            this.Controls.Add(this.lblSubclass);
+            this.Controls.Add(this.cmbSubclass);
+
+            this.Size = new System.Drawing.Size(340, 120);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Frontend/Step3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frontend
+{
+    public partial class Step3 : UserControl
+    {
+        // Placeholder for Class → Subclass map
+        private Dictionary<string, List<string>> classToSubclass = new();
+
+        public Step3()
+        {
+            InitializeComponent();
+            LoadClassData();
+        }
+
+        private void LoadClassData()
+        {
+            // Mock classes
+            cmbClass.Items.AddRange(new object[] { "Fighter", "Wizard", "Cleric" });
+
+            // Mock subclasses
+            classToSubclass["Fighter"] = new List<string> { "Champion", "Battle Master", "Eldritch Knight" };
+            classToSubclass["Wizard"] = new List<string> { "Evocation", "Illusion", "Necromancy" };
+            classToSubclass["Cleric"] = new List<string> { "Life Domain", "War Domain", "Trickery Domain" };
+        }
+
+        private void cmbClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbSubclass.Items.Clear();
+
+            if (cmbClass.SelectedItem is string selectedClass &&
+                classToSubclass.TryGetValue(selectedClass, out var subclasses))
+            {
+                cmbSubclass.Items.AddRange(subclasses.ToArray());
+                if (cmbSubclass.Items.Count > 0)
+                    cmbSubclass.SelectedIndex = 0;
+            }
+        }
+
+        // Pull the selected IDs (for now using name indexes as stand-ins)
+        public int SelectedClassID => cmbClass.SelectedIndex + 1;
+        public int SelectedSubclassID => cmbSubclass.SelectedIndex + 1;
+    }
+}
