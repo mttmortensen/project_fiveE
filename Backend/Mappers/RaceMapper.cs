9f2295be0dd54895299c7c45603ccf62c5e7fa74
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 08:47:53 -0600

    race mapper logic for getting basic race info

---
+++ Backend/Mappers/RaceMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Mappers
+{
+    public class RaceMapper
+    {
+        public List<Race> MapToRaceList(List<Dictionary<string, object>> rawData) 
+        {
+            return rawData.Select(row => new Race
+            {
+                RaceID = Convert.ToInt32(row["RaceID"]),
+                RaceName = row["RaceName"].ToString()
+            }).ToList();
+        }
+    }
+}
