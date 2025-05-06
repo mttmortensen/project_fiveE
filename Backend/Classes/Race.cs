c90d6526c5551e5724baeb646a7c544046be0204
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 10:24:39 -0600

    adding back in RacialProficiencies into Race core for now

--- Backend/Classes/Race.cs
+++ Backend/Classes/Race.cs
@@ -12,6 +12,7 @@
         public List<string> AbilityScoreBonuses { get; set; }
         public List<string> Languages { get; set; }
         public List<string> RacialFeatures { get; set; }
+        public List<string> RacialProficiencies { get; set; } = new List<string>();
         public int Darkvision { get; set; } = 0;
     }
 }
