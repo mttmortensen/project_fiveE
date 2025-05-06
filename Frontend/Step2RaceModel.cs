a685cf61925d965acdd41250deed6998f025e4ba
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 10:11:25 -0600

    introducing a race model to have mapped from api to display

---
+++ Frontend/Step2RaceModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class Step2RaceModel
+    {
+        public int RaceID { get; set; }
+        public string RaceName { get; set; }
+        public string RaceSize { get; set; }
+        public int Speed { get; set; }
+        public List<string> Languages { get; set; }
+        public List<string> RacialFeatures { get; set; }
+        public string Description { get; set; }
+
+        // Just for the dropdown, for conenience. 
+        public override string ToString()
+        {
+            return RaceName; 
+        }
+    }
+}
