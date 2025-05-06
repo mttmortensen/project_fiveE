92aca2a49a096b9541788f5581441462e2a1b4ea
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 12:04:22 -0600

    adding class mapper for get req

---
+++ Backend/Mappers/ClassMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    internal class ClassMapper
+    {
+        public List<Classes> MapToClassList(List<Dictionary<string, object>> rawData) 
+        {
+            return rawData.Select(row => new Classes
+            {
+                ClassID = Convert.ToInt32(row["ClassID"]),
+                ClassName = row["ClassName"].ToString(),
+                HitDie = row["HitDie"].ToString(),
+                PrimaryAbility = row["PrimaryAbility"].ToString(),
+                SavingThrows = JsonSerializer.Deserialize<List<string>>(row["SavingThrows"].ToString()),
+                ClassFeatures = JsonSerializer.Deserialize<List<string>>(row["ClassFeatures"].ToString()),
+            }).ToList();
+        }
+    }
+}
