7726ea5b9e08eb56adeae1af60c7e5bbebaf752f
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 14:52:26 -0600

    adding class model for step 3 with backend now ready

---
+++ Frontend/Step3ClassModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class Step3ClassModel
+    {
+        public int ClassID { get; set; }
+        public string ClassName { get; set; }
+        public string HitDie { get; set; }
+        public string PrimaryAbility { get; set; }
+        public List<string> SavingThrows { get; set; }
+        public List<string> ClassFeatures { get; set; }
+
+        public override string ToString()
+        {
+            return ClassName;
+        }
+    }
+}
