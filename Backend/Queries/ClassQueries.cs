387fd635263c848b1e7ac66e628cf1cef06e3a45
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 11:55:34 -0600

    start of get req for class data

---
+++ Backend/Queries/ClassQueries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Queries
+{
+    public class ClassQueries
+    {
+        public string GetAllClasses => @"
+            SELECT ClassID, ClassName, HitDie, PrimaryAbility, SavingThrows, ClassFeatures
+            FROM Classes
+            ORDER BY ClassName;
+        ";
+    }
+}
