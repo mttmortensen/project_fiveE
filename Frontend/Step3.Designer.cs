3cb2cd031af7d70aaaa1e257518a198ab97bbd23
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 14:54:35 -0600

    updated design to hold just class for right now

--- Frontend/Step3.Designer.cs
+++ Frontend/Step3.Designer.cs
@@ -5,15 +5,21 @@
         private System.ComponentModel.IContainer components = null;
         private Label lblClass;
         private ComboBox cmbClass;
-        private Label lblSubclass;
-        private ComboBox cmbSubclass;
+        private GroupBox grpClassDetails;
+        private Label lblHitDie;
+        private Label lblPrimaryAbility;
+        private Label lblSavingThrows;
+        private Label lblFeatures;
 
         private void InitializeComponent()
         {
             this.lblClass = new Label();
             this.cmbClass = new ComboBox();
-            this.lblSubclass = new Label();
-            this.cmbSubclass = new ComboBox();
+            this.grpClassDetails = new GroupBox();
+            this.lblHitDie = new Label();
+            this.lblPrimaryAbility = new Label();
+            this.lblSavingThrows = new Label();
+            this.lblFeatures = new Label();
 
             this.SuspendLayout();
 
@@ -27,23 +33,43 @@
             this.cmbClass.DropDownStyle = ComboBoxStyle.DropDownList;
             this.cmbClass.SelectedIndexChanged += new System.EventHandler(this.cmbClass_SelectedIndexChanged);
 
-            // Label: Subclass
-            this.lblSubclass.Text = "Select Subclass:";
-            this.lblSubclass.Location = new System.Drawing.Point(20, 60);
+            // GroupBox: Class Details
+            this.grpClassDetails.Text = "Class Details";
+            this.grpClassDetails.Location = new System.Drawing.Point(20, 60);
+            this.grpClassDetails.Size = new System.Drawing.Size(300, 150);
 
-            // ComboBox: Subclass
-            this.cmbSubclass.Location = new System.Drawing.Point(120, 60);
-            this.cmbSubclass.Width = 180;
-            this.cmbSubclass.DropDownStyle = ComboBoxStyle.DropDownList;
+            // Label: Hit Die
+            this.lblHitDie.Text = "Hit Die: ";
+            this.lblHitDie.Location = new System.Drawing.Point(10, 20);
+            this.lblHitDie.AutoSize = true;
 
-            // Add controls
+            // Label: Primary Ability
+            this.lblPrimaryAbility.Text = "Primary Ability: ";
+            this.lblPrimaryAbility.Location = new System.Drawing.Point(10, 40);
+            this.lblPrimaryAbility.AutoSize = true;
+
+            // Label: Saving Throws
+            this.lblSavingThrows.Text = "Saving Throws: ";
+            this.lblSavingThrows.Location = new System.Drawing.Point(10, 60);
+            this.lblSavingThrows.AutoSize = true;
+
+            // Label: Features
+            this.lblFeatures.Text = "Features: ";
+            this.lblFeatures.Location = new System.Drawing.Point(10, 80);
+            this.lblFeatures.AutoSize = true;
+
+            this.grpClassDetails.Controls.Add(this.lblHitDie);
+            this.grpClassDetails.Controls.Add(this.lblPrimaryAbility);
+            this.grpClassDetails.Controls.Add(this.lblSavingThrows);
+            this.grpClassDetails.Controls.Add(this.lblFeatures);
+
             this.Controls.Add(this.lblClass);
             this.Controls.Add(this.cmbClass);
-            this.Controls.Add(this.lblSubclass);
-            this.Controls.Add(this.cmbSubclass);
+            this.Controls.Add(this.grpClassDetails);
 
-            this.Size = new System.Drawing.Size(340, 120);
+            this.Size = new System.Drawing.Size(340, 240);
             this.ResumeLayout(false);
         }
     }
+
 }
