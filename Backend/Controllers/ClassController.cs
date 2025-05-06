35a7ebf3fe99f0af9c64d68ed778ede4bf10a5f4
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 12:09:11 -0600

    logic for class controller now added

--- Backend/Controllers/ClassController.cs
+++ Backend/Controllers/ClassController.cs
@@ -8,5 +8,21 @@ namespace Backend
 {
     public class ClassController
     {
+        private readonly Database _database;
+        private readonly ClassQueries _queries;
+        private readonly ClassMapper _mapper;
+
+        public ClassController(Database database)
+        {
+            _database = database;
+            _queries = new ClassQueries();
+            _mapper = new ClassMapper();
+        }
+
+        public List<Classes> GetAllClasses()
+        {
+            var rawData = _database.GetRawDataFromDatabase(_queries.GetAllClasses);
+            return _mapper.MapToClassList(rawData);
+        }
     }
 }
