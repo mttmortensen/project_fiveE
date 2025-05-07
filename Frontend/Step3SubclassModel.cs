eb6e11643f1a731590fd5dffb254d5690ea934c7
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 21:11:31 -0600

    starting on subclass now for step 3

---
+++ Frontend/Step3SubclassModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class Step3SubclassModel
+    {
+        public int SubclassID { get; set; }
+        public int ClassID { get; set; }
+        public string SubclassName { get; set; }
+        public List<string> BonusSpells { get; set; }
+        public List<string> BonusProficiencies { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return SubclassName;
+        }
+    }
+}
