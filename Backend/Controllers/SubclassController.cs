12da3cabc61658bae458a65978ab61d4a7218ac7
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 19:29:56 -0600

    adding subclass controller

---
+++ Backend/Controllers/SubclassController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SubclassController
+    {
+        private readonly Database _database;
+        private readonly SubclassQueries _queries;
+        private readonly SubclassMapper _mapper;
+        public SubclassController(Database database)
+        {
+            _database = database;
+            _queries = new SubclassQueries();
+            _mapper = new SubclassMapper();
+        }
+
+        public List<Subclass> GetSubclassesByClassId(int classId)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { "@ClassID", classId }
+            };
+            var rawData = _database.GetRawDataFromDatabase(_queries.GetSubclassesByClassId, parameters);
+            return _mapper.MapToSubclassList(rawData);
+        }
+
+    }
+}
