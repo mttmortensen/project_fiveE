e8c6756f33d5f578c0f1e5ae56e9ec29bc41bc14
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-06 19:09:21 -0600

    start on subclass get req logic. start with queries

---
+++ Backend/Queries/SubclassQueries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SubclassQueries
+    {
+        public string GetSubclassesByClassId => @"
+            SELECT SubclassID, ClassID, SubclassName, EntryLevel, SubclassFeatures
+            FROM Subclasses
+            WHERE ClassID = @ClassID
+            ORDER BY SubclassName;
+        ";
+    }
+}
