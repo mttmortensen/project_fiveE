3bcb74cd976e0c3dd658b8696cc8a6a57bd3d0c9
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-08 19:40:05 -0600

    adding queries to get data from to use in

---
+++ Backend/Queries/SubraceQueries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SubraceQueries
+    {
+        public string GetSubracesByRaceId => @"
+            SELECT SubraceID, RaceID, SubraceName, Description, AbilityScoreIncreases, Traits
+            FROM Subraces
+            WHERE RaceID = @RaceID
+            ORDER BY SubraceName;
+        ";
+    }
+}
