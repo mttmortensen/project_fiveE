d827bd0dc83a3aa57260a6221540fb0e29d4d463
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-08 19:34:54 -0600

    adding mapper data from db to obj

---
+++ Backend/Mappers/SubraceMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SubraceMapper
+    {
+        public List<Subrace> MapToSubraceList(List<Dictionary<string, object>> rawData)
+        {
+            return rawData.Select(row => new Subrace
+            {
+                SubraceID = Convert.ToInt32(row["SubraceID"]),
+                RaceID = Convert.ToInt32(row["RaceID"]),
+                SubraceName = row["SubraceName"].ToString(),
+                Description = row["Description"].ToString(),
+                Traits = JsonSerializer.Deserialize<List<string>>(row["Traits"].ToString()) ?? new List<string>(),
+                AbilityScoreIncreases = JsonSerializer.Deserialize<List<string>>(row["AbilityScoreIncreases"].ToString()) ?? new List<string>(),
+            }).ToList();
+        }
+    }
+}
