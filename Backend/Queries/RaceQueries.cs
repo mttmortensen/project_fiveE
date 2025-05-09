8c4b0fb1737b7661ebd1731df6017f19a12c7ec3
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-09 13:56:41 -0600

    updating RaceQueries to hold more for RaceController to use

--- Backend/Queries/RaceQueries.cs
+++ Backend/Queries/RaceQueries.cs
@@ -8,10 +8,74 @@ namespace Backend
 {
     public class RaceQueries
     {
+        // GET: All static race data
         public string GetAllRaces => @"
-            SELECT RaceID, RaceName, RaceSize, Speed, Languages, RacialFeatures, Description
+            SELECT RaceID, RaceName, RaceCreatureType, RaceSize, RaceSpeed, Description
             FROM Races
-            ORDER By RaceName;
+            ORDER BY RaceName;
+        ";
+
+        // GET: Race + CharacterRace data for a single character
+        public string GetCharacterRaceById => @"
+            SELECT 
+                cr.CharRaceID,
+                cr.CharacterID,
+                cr.RaceID,
+                cr.Traits,
+                r.RaceName,
+                r.RaceCreatureType,
+                r.RaceSize,
+                r.RaceSpeed,
+                r.Description
+            FROM CharacterRace cr
+            INNER JOIN Races r ON cr.RaceID = r.RaceID
+            WHERE cr.CharacterID = @CharacterID;
+        ";
+
+        // GET: CharacterRaceSelection for a character
+        public string GetCharacterRaceSelectionById => @"
+            SELECT 
+                CharRaceSelectID,
+                CharacterID,
+                RaceID,
+                SubraceID,
+                Languages,
+                SelectedTraits,
+                SelectedProficiencies,
+                SelectedSpells,
+                SelectedAbilityScoreBonuses
+            FROM CharacterRaceSelection
+            WHERE CharacterID = @CharacterID;
+        ";
+
+        // POST: Insert CharacterRace
+        public string LinkCharacterRace => @"
+            INSERT INTO CharacterRace (CharacterID, RaceID, Traits)
+            VALUES (@CharacterID, @RaceID, @Traits);
+        ";
+
+        // POST: Insert CharacterRaceSelection
+        public string LinkCharacterRaceSelection => @"
+            INSERT INTO CharacterRaceSelection (
+                CharacterID,
+                RaceID,
+                SubraceID,
+                Languages,
+                SelectedTraits,
+                SelectedProficiencies,
+                SelectedSpells,
+                SelectedAbilityScoreBonuses
+            ) VALUES (
+                @CharacterID,
+                @RaceID,
+                @SubraceID,
+                @Languages,
+                @SelectedTraits,
+                @SelectedProficiencies,
+                @SelectedSpells,
+                @SelectedAbilityScoreBonuses
+            );
         ";
     }
 }
+
