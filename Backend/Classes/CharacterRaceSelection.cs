46cf30d43235051716d71c5397c135018eda576d
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-08 22:15:38 -0600

    CRS is the race data needed to be selected by the character instead of given like CR

---
+++ Backend/Classes/CharacterRaceSelection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class CharacterRaceSelection
+    {
+        public int CharRaceSelectID { get; set; }
+        public int CharacterID { get; set; }
+        public int RaceID { get; set; }
+        public int SubRaceID { get; set; }
+
+        public List<string> Languages { get; set; } = new();
+        public List<string> SelectedTraits { get; set; } = new();
+        public List<string> SelectedProficiencies { get; set; } = new();
+        public List<string> SelectedSpells { get; set; } = new();
+        public Dictionary<string, int> SelectedAbilityScoreBonuses { get; set; } = new();
+    }
+
+}
