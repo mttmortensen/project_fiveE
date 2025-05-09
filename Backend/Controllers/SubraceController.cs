17783c0abc33ca42e163cb2fa180366d7d59cb5c
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-08 19:40:27 -0600

    controller made for subrace

---
+++ Backend/Controllers/SubraceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SubraceController
+    {
+        private readonly Database _database;
+        private readonly SubraceQueries _queries;
+        private readonly SubraceMapper _mapper;
+        public SubraceController(Database database)
+        {
+            _database = database;
+            _queries = new SubraceQueries();
+            _mapper = new SubraceMapper();
+        }
+
+        public List<Subrace> GetSubclassesByClassId(int classId)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { "@ClassID", classId }
+            };
+            var rawData = _database.GetRawDataFromDatabase(_queries.GetSubracesByRaceId, parameters);
+            return _mapper.MapToSubraceList(rawData);
+        }
+
+    }
+}
