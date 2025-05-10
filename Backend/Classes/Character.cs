80e4254ba6a0622067a9efdeaa644616cb809d4d
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-10 15:36:08 -0600

    applying updated CharacterRaceOptions and Selection back to Character

--- Backend/Classes/Character.cs
+++ Backend/Classes/Character.cs
@@ -49,7 +49,6 @@
         // Linked Tables (Composition over Duplication)
         public Race Race { get; set; }
         public Subrace Subrace { get; set; }  
-        public CharacterRaceOptions CharacterRace { get; set; }
         public CharacterRaceSelection CharacterRaceSelection { get; set; }
 
         public Classes Classes { get; set; }
