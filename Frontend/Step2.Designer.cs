73170da1bb7092e92357b67c62038741d7870af1
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-13 15:18:54 -0600

    designer now matches the updated logic for Step2.cs

--- Frontend/Step2.Designer.cs
+++ Frontend/Step2.Designer.cs
@@ -1,20 +1,28 @@
-﻿namespace Frontend
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Frontend
 {
     partial class Step2
     {
-        private GroupBox grpRaceDetails;
-        private Label lblSize;
-        private Label lblSpeed;
-        private Label lblLanguages;
-        private Label lblFeatures;
-        private TextBox txtDescription;
-
-        private System.ComponentModel.IContainer components = null;
         private Label lblRace;
         private ComboBox cmbRace;
-        private CheckBox chkDarkvision;
+
+        private Label lblSize;
+        private Label lblSpeed;
+        private Label lblCreatureType;
+        private Label lblDarkvision;
+        private Label lblDescription;
+
+        private Label lblTraits;
+        private CheckedListBox lstTraits;
+
+        private Label lblLanguages;
+        private CheckedListBox lstLanguages;
+
         private Label lblProficiencies;
         private CheckedListBox lstProficiencies;
+
         private Label lblBonusStat1;
         private ComboBox cmbBonusStat1;
         private Label lblBonusStat2;
@@ -24,20 +32,21 @@
         {
             lblRace = new Label();
             cmbRace = new ComboBox();
-            chkDarkvision = new CheckBox();
+            lblSize = new Label();
+            lblSpeed = new Label();
+            lblCreatureType = new Label();
+            lblDarkvision = new Label();
+            lblDescription = new Label();
+            lblTraits = new Label();
+            lstTraits = new CheckedListBox();
+            lblLanguages = new Label();
+            lstLanguages = new CheckedListBox();
             lblProficiencies = new Label();
             lstProficiencies = new CheckedListBox();
             lblBonusStat1 = new Label();
             cmbBonusStat1 = new ComboBox();
             lblBonusStat2 = new Label();
             cmbBonusStat2 = new ComboBox();
-            grpRaceDetails = new GroupBox();
-            lblSize = new Label();
-            lblSpeed = new Label();
-            lblLanguages = new Label();
-            lblFeatures = new Label();
-            txtDescription = new TextBox();
-            grpRaceDetails.SuspendLayout();
             SuspendLayout();
             // 
             // lblRace
@@ -56,138 +65,139 @@
             cmbRace.Size = new Size(180, 23);
             cmbRace.TabIndex = 1;
             // 
-            // chkDarkvision
+            // lblSize
             // 
-            chkDarkvision.Location = new Point(20, 257);
-            chkDarkvision.Name = "chkDarkvision";
-            chkDarkvision.Size = new Size(104, 24);
-            chkDarkvision.TabIndex = 2;
-            chkDarkvision.Text = "Darkvision";
+            lblSize.Location = new Point(20, 55);
+            lblSize.Name = "lblSize";
+            lblSize.Size = new Size(200, 20);
+            lblSize.TabIndex = 2;
+            // 
+            // lblSpeed
+            // 
+            lblSpeed.Location = new Point(20, 75);
+            lblSpeed.Name = "lblSpeed";
+            lblSpeed.Size = new Size(200, 20);
+            lblSpeed.TabIndex = 4;
+            // 
+            // lblCreatureType
+            // 
+            lblCreatureType.Location = new Point(20, 95);
+            lblCreatureType.Name = "lblCreatureType";
+            lblCreatureType.Size = new Size(300, 20);
+            lblCreatureType.TabIndex = 5;
+            // 
+            // lblDarkvision
+            // 
+            lblDarkvision.Location = new Point(20, 115);
+            lblDarkvision.Name = "lblDarkvision";
+            lblDarkvision.Size = new Size(400, 23);
+            lblDarkvision.TabIndex = 6;
+            // 
+            // lblDescription
+            // 
+            lblDescription.Location = new Point(20, 135);
+            lblDescription.Name = "lblDescription";
+            lblDescription.Size = new Size(500, 23);
+            lblDescription.TabIndex = 7;
+            // 
+            // lblTraits
+            // 
+            lblTraits.Location = new Point(20, 145);
+            lblTraits.Name = "lblTraits";
+            lblTraits.Size = new Size(100, 23);
+            lblTraits.TabIndex = 7;
+            lblTraits.Text = "Available Traits:";
+            // 
+            // lstTraits
+            // 
+            lstTraits.Location = new Point(20, 171);
+            lstTraits.Name = "lstTraits";
+            lstTraits.Size = new Size(280, 58);
+            lstTraits.TabIndex = 8;
+            // 
+            // lblLanguages
+            // 
+            lblLanguages.Location = new Point(20, 232);
+            lblLanguages.Name = "lblLanguages";
+            lblLanguages.Size = new Size(190, 23);
+            lblLanguages.TabIndex = 9;
+            lblLanguages.Text = "Available Languages:";
+            // 
+            // lstLanguages
+            // 
+            lstLanguages.Location = new Point(20, 258);
+            lstLanguages.Name = "lstLanguages";
+            lstLanguages.Size = new Size(280, 58);
+            lstLanguages.TabIndex = 10;
             // 
             // lblProficiencies
             // 
-            lblProficiencies.Location = new Point(20, 231);
+            lblProficiencies.Location = new Point(20, 319);
             lblProficiencies.Name = "lblProficiencies";
-            lblProficiencies.Size = new Size(100, 23);
-            lblProficiencies.TabIndex = 3;
-            lblProficiencies.Text = "Racial Proficiencies:";
+            lblProficiencies.Size = new Size(200, 23);
+            lblProficiencies.TabIndex = 11;
+            lblProficiencies.Text = "Available Proficiencies:";
             // 
             // lstProficiencies
             // 
-            lstProficiencies.CheckOnClick = true;
-            lstProficiencies.Location = new Point(20, 287);
+            lstProficiencies.Location = new Point(20, 345);
             lstProficiencies.Name = "lstProficiencies";
             lstProficiencies.Size = new Size(280, 58);
-            lstProficiencies.TabIndex = 4;
+            lstProficiencies.TabIndex = 12;
             // 
             // lblBonusStat1
             // 
-            lblBonusStat1.Location = new Point(20, 353);
+            lblBonusStat1.Location = new Point(20, 429);
             lblBonusStat1.Name = "lblBonusStat1";
             lblBonusStat1.Size = new Size(100, 23);
-            lblBonusStat1.TabIndex = 5;
+            lblBonusStat1.TabIndex = 13;
             lblBonusStat1.Text = "Select +2 Bonus:";
             // 
             // cmbBonusStat1
             // 
             cmbBonusStat1.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbBonusStat1.Location = new Point(140, 353);
+            cmbBonusStat1.Location = new Point(140, 429);
             cmbBonusStat1.Name = "cmbBonusStat1";
             cmbBonusStat1.Size = new Size(160, 23);
-            cmbBonusStat1.TabIndex = 6;
+            cmbBonusStat1.TabIndex = 14;
             // 
             // lblBonusStat2
             // 
-            lblBonusStat2.Location = new Point(20, 393);
+            lblBonusStat2.Location = new Point(20, 459);
             lblBonusStat2.Name = "lblBonusStat2";
             lblBonusStat2.Size = new Size(100, 23);
-            lblBonusStat2.TabIndex = 7;
+            lblBonusStat2.TabIndex = 15;
             lblBonusStat2.Text = "Select +1 Bonus:";
             // 
             // cmbBonusStat2
             // 
             cmbBonusStat2.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbBonusStat2.Location = new Point(140, 393);
+            cmbBonusStat2.Location = new Point(140, 459);
             cmbBonusStat2.Name = "cmbBonusStat2";
             cmbBonusStat2.Size = new Size(160, 23);
-            cmbBonusStat2.TabIndex = 8;
-            // 
-            // grpRaceDetails
-            // 
-            grpRaceDetails.Controls.Add(lblSize);
-            grpRaceDetails.Controls.Add(lblSpeed);
-            grpRaceDetails.Controls.Add(lblLanguages);
-            grpRaceDetails.Controls.Add(lblFeatures);
-            grpRaceDetails.Controls.Add(txtDescription);
-            grpRaceDetails.Location = new Point(20, 45);
-            grpRaceDetails.Name = "grpRaceDetails";
-            grpRaceDetails.Size = new Size(300, 180);
-            grpRaceDetails.TabIndex = 9;
-            grpRaceDetails.TabStop = false;
-            grpRaceDetails.Text = "Race Details";
-            // 
-            // lblSize
-            // 
-            lblSize.AutoSize = true;
-            lblSize.Location = new Point(10, 20);
-            lblSize.Name = "lblSize";
-            lblSize.Size = new Size(33, 15);
-            lblSize.TabIndex = 0;
-            lblSize.Text = "Size: ";
-            // 
-            // lblSpeed
-            // 
-            lblSpeed.AutoSize = true;
-            lblSpeed.Location = new Point(10, 40);
-            lblSpeed.Name = "lblSpeed";
-            lblSpeed.Size = new Size(45, 15);
-            lblSpeed.TabIndex = 1;
-            lblSpeed.Text = "Speed: ";
-            // 
-            // lblLanguages
-            // 
-            lblLanguages.AutoSize = true;
-            lblLanguages.Location = new Point(10, 60);
-            lblLanguages.Name = "lblLanguages";
-            lblLanguages.Size = new Size(70, 15);
-            lblLanguages.TabIndex = 2;
-            lblLanguages.Text = "Languages: ";
-            // 
-            // lblFeatures
-            // 
-            lblFeatures.AutoSize = true;
-            lblFeatures.Location = new Point(10, 80);
-            lblFeatures.Name = "lblFeatures";
-            lblFeatures.Size = new Size(57, 15);
-            lblFeatures.TabIndex = 3;
-            lblFeatures.Text = "Features: ";
-            // 
-            // txtDescription
-            // 
-            txtDescription.Location = new Point(10, 98);
-            txtDescription.Multiline = true;
-            txtDescription.Name = "txtDescription";
-            txtDescription.ReadOnly = true;
-            txtDescription.ScrollBars = ScrollBars.Vertical;
-            txtDescription.Size = new Size(275, 70);
-            txtDescription.TabIndex = 4;
+            cmbBonusStat2.TabIndex = 16;
             // 
             // Step2
             // 
             Controls.Add(lblRace);
             Controls.Add(cmbRace);
-            Controls.Add(chkDarkvision);
+            Controls.Add(lblSize);
+            Controls.Add(lblSpeed);
+            Controls.Add(lblCreatureType);
+            Controls.Add(lblDarkvision);
+            Controls.Add(lblDescription);
+            Controls.Add(lblTraits);
+            Controls.Add(lstTraits);
+            Controls.Add(lblLanguages);
+            Controls.Add(lstLanguages);
             Controls.Add(lblProficiencies);
             Controls.Add(lstProficiencies);
             Controls.Add(lblBonusStat1);
             Controls.Add(cmbBonusStat1);
             Controls.Add(lblBonusStat2);
             Controls.Add(cmbBonusStat2);
-            Controls.Add(grpRaceDetails);
             Name = "Step2";
-            Size = new Size(340, 480);
-            grpRaceDetails.ResumeLayout(false);
-            grpRaceDetails.PerformLayout();
+            Size = new Size(340, 536);
             ResumeLayout(false);
         }
     }
